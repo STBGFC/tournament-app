e9b112de51f4be1d6ea38ea06d216182e21c3d52
Darren Davison <darren@davisononline.org>
2015-04-17 21:04:26 +0100

    Move old server side table calculation code to front end.

--- web/app/scripts/modules/tournament.js
+++ web/app/scripts/modules/tournament.js
@@ -55,12 +55,81 @@ angular
                 var res = compResults[i];
                 if ('group' in res.competition) {
                     competition.groups[res.competition.group - 1].results.push(res);
+                    updateTable(res, competition.groups[res.competition.group - 1].table);
                 }
                 else {
                     competition.results.push(res);
                 }
             }
         });
+
+        /*
+         * given a single result and a current table (which can be the empty array []),
+         * return an updated set of entries
+         */
+        var updateTable = function(newResult, table) {
+            var hg = newResult.homeGoals;
+            var ag = newResult.awayGoals;
+            var ht = findEntryInTable(newResult.homeTeam, table);
+            var at = findEntryInTable(newResult.awayTeam, table);
+            applyRes(ht, hg, ag);
+            applyRes(at, ag, hg);
+            table.sort(leagueComparator);
+        };
+
+        var findEntryInTable = function(name, table) {
+            for (var i = 0; i < table.length; i++) {
+                if (table[i].name === name) return table[i];
+            }
+
+            var entry = {played: 0, won: 0, drawn: 0, lost: 0, goalsFor: 0, goalsAgainst: 0, points: 0};
+            entry.name = name;
+            table.push(entry);
+            return entry;
+        };
+
+        /*
+         * function to apply a result to an existing table of entries in order to
+         * update them.  The entries can then be resorted using the comparator
+         */
+        var applyRes = function(entry, myGoals, yourGoals) {
+            entry.played++;
+            entry.won += (myGoals > yourGoals ? 1 : 0);
+            entry.drawn += (myGoals === yourGoals ? 1 : 0);
+            entry.lost += (myGoals < yourGoals ? 1 : 0);
+            entry.goalsFor += myGoals;
+            entry.goalsAgainst += yourGoals;
+            entry.points += (myGoals > yourGoals ? 3 : (myGoals === yourGoals ? 1 : 0));
+            return entry;
+        };
+
+        /*
+         * comparator to sort an array of table entries using the most common mechanics
+         * for European sports leagues (by points, then GD, then goals scored, then wins)
+         */
+        var leagueComparator = function(a, b) {
+            // points
+            if (a.points > b.points) return -1;
+            if (a.points < b.points) return 1;
+
+            // GD
+            var aGD = a.goalsFor - a.goalsAgainst;
+            var bGD = b.goalsFor - b.goalsAgainst;
+            if (aGD > bGD) return -1;
+            if (aGD < bGD) return 1;
+
+            // scored
+            if (a.goalsFor > b.goalsFor) return -1;
+            if (a.goalsFor < b.goalsFor) return 1;
+
+            // wins
+            if (a.won > b.won) return -1;
+            if (a.won < b.won) return 1;
+
+            // name
+            return (a.name < b.name);
+        };
+
 
         // assign local setup to $scope
         $scope.competition = competition;
