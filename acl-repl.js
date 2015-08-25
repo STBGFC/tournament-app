ba870ac181845f3ebbdafd198ee1c3d530efada3
Darren Davison <darren@davisononline.org>
2015-08-25 21:38:58 +0100

    cosmetic polishing

--- acl-repl.js
+++ acl-repl.js
@@ -1,29 +1,30 @@
 /*
  * open a node REPL and then .load this file to set up the
  * ACL and connection to the backend.  Ensure you specify the
- * mongo URI in the environment for the REPL
+ * mongo URI in the environment for the REPL.
+ *
+ * Once users are created (see /qdr.html) they can be assigned
+ * roles inside this REPL with:
+ *   acl.addUserRoles('joed', 'editor')
  */
 
 var acl = require('acl');
-var mongodb = require('mongodb'); 
+var mongodb = require('mongodb');
 var mongoUri = process.env.STBGFC_MONGO_URI || 'mongodb://localhost/tournamentApp';
 var User = require('./server/models/User');
 
-mongodb.connect(mongoUri, function(error, db) {
+mongodb.connect(mongoUri, function (error, db) {
     var mongoBackend = new acl.mongodbBackend(db, 'acl_');
     acl = new acl(mongoBackend);
 });
 
 
-var resources = ['/results','/news','/feedbacks','/tournaments'];
+var resources = ['/results', '/news', '/feedbacks', '/tournaments'];
 // pass a users array..
-var showPerms = function(users, resources) {
-    for (var i = 0; i < users.length; i++) {
-        acl.allowedPermissions(users[i], resources, function(err,perms) {
-            console.log(perms);
-        });
-    }
+var showPerms = function (user) {
+    acl.allowedPermissions(user, resources, function (err, perms) {
+        console.log(perms);
+    });
 };
 
-console.log('Try  showPerms(<users>, resources)');
-
+console.log('Try  "showPerms(user)"  or  "acl.addUserRoles(\'foo@example.com\', \'editor\')"');
