ac4abc3d8774139fe9d05cbb6eeb96e637fb9cb1
Darren Davison <darren@davisononline.org>
2018-11-08 13:30:18 +0000

    Re-order imports

--- src/main.js
+++ src/main.js
@@ -3,23 +3,22 @@ import BootstrapVue from 'bootstrap-vue'
 import { Navbar, Tabs } from 'bootstrap-vue/es/components';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue)
-Vue.use(Navbar)
-Vue.use(Tabs)
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faClock, faTag, faMap, faFutbol, faRunning } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add([faClock, faTag, faMap, faFutbol, faRunning])
-Vue.component('fa-icon', FontAwesomeIcon)
-
 import App from '@/components/App.vue'
 import router from '@/router'
 import store from '@/store'
 
-//import '@/assets/branding.scss';
+import '@/assets/branding.scss';
 
 //Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(Navbar)
+Vue.use(Tabs)
+library.add([faClock, faTag, faMap, faFutbol, faRunning])
+Vue.component('fa-icon', FontAwesomeIcon)
 
 new Vue({
   router,
